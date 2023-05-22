5856627709d9caefa60dd1c6e2d205aa03838b52
rfrias8 <rfrias8@gmail.com>
2023-05-21 22:08:06 -0500

    Add Edit screen

---
+++ public/react/components/AddEditQuote.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react';
+import image from "../components/images/new-sponge.jpg";
+
+
+
+export const AddEditQuote = () => {
+    return (
+
+            <div style={{display:"flex",justifyContent:"center", alignItems:"center", height:"1000px", backgroundImage: `url(${image})`,backgroundSize:"100%", backgroundRepeat:"no-repeat"}}>
+                <form>
+                    <div style={{border: "solid",padding: "150px", backgroundColor:"grey", opacity:"0.85"}}>
+                        <label style={{margin:"300px", fontSize: "30px"}}>New/Update Quote:
+                            <input style={{fontSize:"20px",paddingRight:"100px", margin:"5px"}} type="text" />
+                            <button>Enter</button>
+                        </label>
+                    </div>
+                </form>
+            </div>
+ 
+      );
+  } 
+
