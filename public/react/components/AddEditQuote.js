56ef46ee39793ccaeec9aa7171a702075089cfe5
rfrias8 <rfrias8@gmail.com>
2023-05-31 19:50:24 -0500

    Changed font size

--- public/react/components/AddEditQuote.js
+++ public/react/components/AddEditQuote.js
@@ -9,7 +9,7 @@ export const AddEditQuote = () => {
             <div style={{display:"flex",justifyContent:"center", alignItems:"center", height:"1000px", backgroundImage: `url(${image})`,backgroundSize:"100%", backgroundRepeat:"no-repeat"}}>
                 <form>
                     <div style={{border: "solid",padding: "150px", backgroundColor:"grey", opacity:"0.85"}}>
-                        <label style={{margin:"300px", fontSize: "30px"}}>New/Update Quote:
+                        <label style={{margin:"300px", fontSize: "18px"}}>New/Update Quote:
                             <input style={{fontSize:"20px",paddingRight:"100px", margin:"5px"}} type="text" />
                             <button>Enter</button>
                         </label>
